571824e3aed279c45cd24bed0ddc00c4a04e9b53
Carlos Leonardo Emanuele <carlos@stuvi.space>
2024-03-26 17:28:21 -0400

    fixed payment button

--- components/admin/payment-connection-status.tsx
+++ components/admin/payment-connection-status.tsx
@@ -6,7 +6,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { singleLevelNestedRoutes } from "@/lib/routes";
+import { routes, singleLevelNestedRoutes } from "@/lib/routes";
 import { cn } from "@/lib/utils";
 import { hasConnectedStripeAccount } from "@/server-actions/stripe/account";
 import { AlertCircle, CheckCircle2, ChevronDown } from "lucide-react";
@@ -17,19 +17,26 @@ export const PaymentConnectionStatus = async () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger
-        className={cn(
-          "flex items-center gap-1 justify-center rounded-md border py-1 px-3 text-sm text-center",
-          connectedStripeAccount
-            ? "bg-green-100 border-green-500 text-green-700"
-            : "bg-yellow-100 border-yellow-500 text-yellow-700"
-        )}
+      <Link
+        href={singleLevelNestedRoutes.account.payments}
       >
-        {connectedStripeAccount?  <CheckCircle2 size={16} /> : <AlertCircle size={16} />}
-        <p className="font-bold">Payments:</p>
-        <p>{connectedStripeAccount ? "Connected" : "Not connected"}</p>
-        {/* <ChevronDown size={18} /> */}
-      </DropdownMenuTrigger>
+        <DropdownMenuTrigger
+          className={cn(
+            "flex items-center gap-1 justify-center rounded-md border py-1 px-3 text-sm text-center",
+            connectedStripeAccount
+              ? "bg-green-100 border-green-500 text-green-700"
+              : "bg-yellow-100 border-yellow-500 text-yellow-700"
+          )}
+        >
+
+          {connectedStripeAccount ? <CheckCircle2 size={16} /> : <AlertCircle size={16} />}
+          <p className="font-bold">Payments:</p>
+          <p>{connectedStripeAccount ? "Connected" : "Not connected"}</p>
+          {/* <ChevronDown size={18} /> */}
+        </DropdownMenuTrigger>
+
+      </Link>
+
       {/* <DropdownMenuContent>
         <DropdownMenuLabel>Payments</DropdownMenuLabel>
         <DropdownMenuSeparator />
